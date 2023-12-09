3b0b5745e34151984cfd3f78b163ebaa248bd339
Luke Annison <lukeannison@gmail.com>
2023-12-09 12:17:29 +0000

    added sync responsder for nt / st, added bulk mint, changed encoder activate, changed mocks to af folder

--- contracts/NewcoinEncoder.sol
+++ contracts/NewcoinEncoder.sol
@@ -15,13 +15,20 @@ interface IActivationFunctionAsync {
     function activate(uint256 activationFunctionId) external;
 }
 
-
 /// @title NewcoinEncoder
 /// @notice The handler contract that is authorized to mint NeuralTokens and Watts by registering approved ValidationFunctions.
 contract NewcoinEncoder is Initializable, OwnableUpgradeable {
-
     /// @dev Enum to represent the type of Watt.
-    enum WattType { NONE, CWATT, XWATT, LWATT, NWATT, PWATT, SWATT, VWATT }
+    enum WattType {
+        NONE,
+        CWATT,
+        XWATT,
+        LWATT,
+        NWATT,
+        PWATT,
+        SWATT,
+        VWATT
+    }
 
     /// @dev Struct to represent an activationFunction.
     struct ActivationFunction {
@@ -46,7 +53,11 @@ contract NewcoinEncoder is Initializable, OwnableUpgradeable {
     EnergyMinterMock public poC;
 
     /// @dev Event emitted when a new activationFunction is registered.
-    event ActivationFunctionRegistered(uint256 activationFunctionId, address developer, string context);
+    event ActivationFunctionRegistered(
+        uint256 activationFunctionId,
+        address developer,
+        string context
+    );
 
     /// @dev Event emitted when an activationFunction is approved.
     event ActivationFunctionApproved(uint256 activationFunctionId);
@@ -59,7 +70,7 @@ contract NewcoinEncoder is Initializable, OwnableUpgradeable {
     /// @dev Contract constructor that sets the owner and the EnergyMinterMock instance.
     /// @param _poC Address of the EnergyMinterMock contract.
     function initialize(address _poC) public initializer {
-      __Ownable_init();
+        __Ownable_init();
         poC = EnergyMinterMock(_poC);
     }
 
@@ -80,7 +91,6 @@ contract NewcoinEncoder is Initializable, OwnableUpgradeable {
         uint256 _weightInWatt,
         bool _isAsync
     ) external returns (uint256) {
-
         require(_wattType != WattType.NONE, "Invalid watt type");
 
         ActivationFunction memory newActivationFunction = ActivationFunction({
@@ -96,14 +106,23 @@ contract NewcoinEncoder is Initializable, OwnableUpgradeable {
         });
 
         activationFunctions.push(newActivationFunction);
-        emit ActivationFunctionRegistered(activationFunctions.length - 1, msg.sender, _context);
+        emit ActivationFunctionRegistered(
+            activationFunctions.length - 1,
+            msg.sender,
+            _context
+        );
         return activationFunctions.length - 1;
     }
 
     /// @dev Function for the contract owner to approve an activationFunction for a specific badge ID.
     /// @param activationFunctionId The ID of the activationFunction to approve.
-    function approveActivationFunction(uint256 activationFunctionId) external onlyOwner {
-        require(activationFunctionId < activationFunctions.length, "Invalid activationFunction ID");
+    function approveActivationFunction(
+        uint256 activationFunctionId
+    ) external onlyOwner {
+        require(
+            activationFunctionId < activationFunctions.length,
+            "Invalid activationFunction ID"
+        );
         activationFunctions[activationFunctionId].approved = true;
         emit ActivationFunctionApproved(activationFunctionId);
     }
@@ -111,39 +130,90 @@ contract NewcoinEncoder is Initializable, OwnableUpgradeable {
     /// @dev Function to check if a badge can be minted based on its activationFunction.
     /// @param activationFunctionId The ID of the activationFunction to check.
     /// @return True if the activationFunction is approved, false otherwise.
-    function canMint(uint256 activationFunctionId) external view returns (bool) {
+    function canMint(
+        uint256 activationFunctionId
+    ) external view returns (bool) {
         return activationFunctions[activationFunctionId].approved;
     }
 
     /// @dev Function to mint EnergyMinterMock tokens when an activationFunction is met.
     /// @param activationFunctionId The ID of the activationFunction to mint tokens for.
-    function activate(uint256 activationFunctionId) external {
-        require(activationFunctions[activationFunctionId].approved, "activationFunction not approved");
-        emit DebugAddress(activationFunctions[activationFunctionId].addrss);
 
+    function activate(
+        uint256 activationFunctionId,
+        address recipient
+    ) external {
+        if (recipient == address(0)) {
+            recipient = tx.origin;
+        }
+        ActivationFunction storage af = activationFunctions[
+            activationFunctionId
+        ];
+        require(af.approved, "ActivationFunction not approved");
 
-        if (activationFunctions[activationFunctionId].isAsync) {
-            IActivationFunctionAsync af = IActivationFunctionAsync(activationFunctions[activationFunctionId].addrss);
-            af.activate(activationFunctionId);
+        uint256 amountToMint;
+        if (af.isAsync) {
+            // Handle asynchronous activation function
+            IActivationFunctionAsync asyncAF = IActivationFunctionAsync(
+                af.addrss
+            );
+            asyncAF.activate(activationFunctionId);
         } else {
-            IActivationFunctionSync af = IActivationFunctionSync(activationFunctions[activationFunctionId].addrss);
-            af.activate();
-            require(af.activate(), "ActivationFunction condition not met");
-            uint256 amountToMint = activationFunctions[activationFunctionId].weightInWatt * activationFunctions[activationFunctionId].multiplier;
-            uint8 mintId = uint8(activationFunctions[activationFunctionId].wattType);
-            poC.mint(tx.origin, mintId, amountToMint);
+            // Handle synchronous activation function
+            IActivationFunctionSync syncAF = IActivationFunctionSync(af.addrss);
+            (bool success, uint256 returnedAmount) = syncAF.activate();
+            require(success, "ActivationFunction condition not met");
+
+            if (returnedAmount > 0) {
+                // Use the amount returned by the sync activation function
+                amountToMint = returnedAmount;
+            } else {
+                // Fallback to the existing logic
+                amountToMint = af.weightInWatt * af.multiplier;
+            }
+        }
+
+        uint8 mintId = uint8(af.wattType);
+        poC.mint(recipient, mintId, amountToMint);
+    }
+
+    function bulkMintToAddresses(
+        uint256[] memory activationFunctionIds,
+        address[] memory recipients,
+        uint256[] memory amounts
+    ) external onlyOwner {
+        require(
+            activationFunctionIds.length == recipients.length &&
+                recipients.length == amounts.length,
+            "Arrays must be of the same length"
+        );
+        for (uint i = 0; i < recipients.length; i++) {
+            require(
+                activationFunctions[activationFunctionIds[i]].approved,
+                "Activation function not approved"
+            );
+            uint8 mintId = uint8(
+                activationFunctions[activationFunctionIds[i]].wattType
+            );
+            poC.mint(recipients[i], mintId, amounts[i]);
         }
     }
 
     /// @dev Callback function to mint tokens from asynchronous oracle response.
-    /// @param conditionMet bool for the condition being met 
+    /// @param conditionMet bool for the condition being met
     /// @param activationFunctionId the stored id in the ActivationFunction id to register correctly here.
-    function oracleResponse(bool conditionMet, uint256 activationFunctionId) external {
+    function oracleResponse(
+        bool conditionMet,
+        uint256 activationFunctionId
+    ) external {
         if (conditionMet) {
-            uint256 amountToMint = activationFunctions[activationFunctionId].weightInWatt * activationFunctions[activationFunctionId].multiplier;
-            uint8 mintId = uint8(activationFunctions[activationFunctionId].wattType);
+            uint256 amountToMint = activationFunctions[activationFunctionId]
+                .weightInWatt *
+                activationFunctions[activationFunctionId].multiplier;
+            uint8 mintId = uint8(
+                activationFunctions[activationFunctionId].wattType
+            );
             poC.mint(tx.origin, mintId, amountToMint);
         }
     }
 }
-
