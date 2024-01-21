4eac587c466a8ddb01470d7df88a6d11d07a89d6
Vectoshi Eigenvector Newkamoto <112469623+vectoshi@users.noreply.github.com>
2024-01-21 17:23:56 +0100

    Delete contracts directory

--- contracts/interfaces/IERC1155Burnable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
-
-import {IERC1155Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
-
-interface IERC1155Burnable is IERC1155Upgradeable {
-    function burn(address account, uint256 id, uint256 value) external;
-}
--- contracts/interfaces/ISparkMinter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
-
-interface ISparkMinter {
-    function mint(address account, uint256 amount) external;
-
-    function award(address account, uint256 amount, address recipient) external;
-}
